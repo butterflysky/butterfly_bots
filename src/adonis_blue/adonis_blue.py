3d022f2b1257003a6846b95458fab79d1f85e454
Lina Edwards <lina@butterflysky.dev>
2021-08-08 19:05:12 -0700

    reformatting

--- src/adonis_blue/adonis_blue.py
+++ src/adonis_blue/adonis_blue.py
@@ -8,12 +8,10 @@ from dotenv import load_dotenv
 
 import butterfly_bot.cogs
 
-
 load_dotenv()
 
-
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger('adonis_blue')
+logger = logging.getLogger("adonis_blue")
 
 intents = discord.Intents.default()
 intents.members = True
@@ -21,8 +19,12 @@ intents.typing = False
 intents.presences = False
 
 description = "butterfly bot alpha"
-adonis_blue = commands.Bot(command_prefix=commands.when_mentioned_or('!'), description=description, intents=intents,
-                           strip_after_prefix=True)
+adonis_blue = commands.Bot(
+    command_prefix=commands.when_mentioned_or("!"),
+    description=description,
+    intents=intents,
+    strip_after_prefix=True,
+)
 
 adonis_blue.add_cog(butterfly_bot.cogs.OpenAIBot(adonis_blue))
-adonis_blue.run(os.getenv('DISCORD_API_KEY'))
+adonis_blue.run(os.getenv("DISCORD_API_KEY"))
