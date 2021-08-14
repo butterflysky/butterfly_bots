10ed48a1b7cd3013fa308914530e7441e0986b64
Lina Edwards <lina@butterflysky.dev>
2021-08-13 20:18:48 -0700

    reformat utils.py

--- src/lib/butterfly_bot/utils.py
+++ src/lib/butterfly_bot/utils.py
@@ -6,16 +6,18 @@ class ResponseTarget(Enum):
     LAST_MESSAGE = 2
 
 
-async def send_responses(ctx, parts, code_block=True, response_target=ResponseTarget.LAST_MESSAGE):
+async def send_responses(
+    ctx, parts, code_block=True, response_target=ResponseTarget.LAST_MESSAGE
+):
     respond_to = ctx.message
     if isinstance(parts, str):
         parts = [parts]
     for part in parts:
         if code_block:
             part = f"```{part}```"
-        last_message = await ctx.send(content=part,
-                                      reference=respond_to,
-                                      mention_author=True)
+        last_message = await ctx.send(
+            content=part, reference=respond_to, mention_author=True
+        )
         if response_target is ResponseTarget.LAST_MESSAGE:
             respond_to = last_message
 
@@ -26,10 +28,10 @@ def pretty_time_delta(seconds):
     hours, seconds = divmod(seconds, 3600)
     minutes, seconds = divmod(seconds, 60)
     if days > 0:
-        return '%dd%dh%dm%ds' % (days, hours, minutes, seconds)
+        return "%dd%dh%dm%ds" % (days, hours, minutes, seconds)
     elif hours > 0:
-        return '%dh%dm%ds' % (hours, minutes, seconds)
+        return "%dh%dm%ds" % (hours, minutes, seconds)
     elif minutes > 0:
-        return '%dm%ds' % (minutes, seconds)
+        return "%dm%ds" % (minutes, seconds)
     else:
-        return '%ds' % (seconds,)
+        return "%ds" % (seconds,)
