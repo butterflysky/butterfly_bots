3e788d08a663a29cc394d51347b59f048ae67b67
Lina Edwards <lina@butterflysky.dev>
2021-08-13 20:16:29 -0700

    reformat setup.py

--- src/lib/setup.py
+++ src/lib/setup.py
@@ -2,13 +2,14 @@
 
 from distutils.core import setup
 
-setup(name='butterfly_bot',
-      version='0.1.0',
-      description="Library for building butterfly's discord chat bots",
-      author='Lina Edwards',
-      author_email='lina@butterflysky.dev',
-      packages=['butterfly_bot'],
-      install_requires=[
-          'openai>=0.10.2',
-      ]
-      )
+setup(
+    name="butterfly_bot",
+    version="0.1.0",
+    description="Library for building butterfly's discord chat bots",
+    author="Lina Edwards",
+    author_email="lina@butterflysky.dev",
+    packages=["butterfly_bot"],
+    install_requires=[
+        "openai>=0.10.2",
+    ],
+)
