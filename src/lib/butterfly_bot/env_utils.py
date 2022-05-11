6b03aa353a70532294167e191edec30149e123a9
Lina Edwards <lina@butterflysky.dev>
2022-05-11 09:10:46 -0700

    add logging to environment loader

--- src/lib/butterfly_bot/env_utils.py
+++ src/lib/butterfly_bot/env_utils.py
@@ -1,7 +1,10 @@
 import contextlib
+import logging
 import os
 
 from dotenv import load_dotenv
+
+logger = logging.getLogger(__name__)
 
 
 def load_environment():
@@ -12,6 +15,8 @@ def load_environment():
         for env_file in env_files:
             # strip _FILE to get key name
             key = env_file[:-5]
+
+            logger.info(f"loading environment var {key} from {env_file}")
             with open(env_file, "r") as f:
                 os.environ[key] = f.read()
 
