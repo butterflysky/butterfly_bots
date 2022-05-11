46f8d740d6774de27666d651ad9333191de3a6ae
Lina Edwards <lina@butterflysky.dev>
2022-05-11 11:05:20 -0700

    default logging to debug

--- src/adonis_blue/adonis_blue.py
+++ src/adonis_blue/adonis_blue.py
@@ -9,7 +9,7 @@ from discord.ext import commands
 from discord_slash import SlashCommand
 from version import get_bot_version
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger("adonis_blue")
 
 load_environment()
