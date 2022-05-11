56cc7590ce6dc3b298aad6eecfa57b4d7f757f6f
Lina Edwards <lina@butterflysky.dev>
2022-05-11 09:26:31 -0700

    fix bug in environment loading

--- src/lib/butterfly_bot/env_utils.py
+++ src/lib/butterfly_bot/env_utils.py
@@ -8,11 +8,12 @@ logger = logging.getLogger(__name__)
 
 def load_environment():
     load_dotenv()
-    env_files = [os.getenv(key) for key in os.environ.keys() if key.endswith("_FILE")]
+    env_file_keys = [key for key in os.environ.keys() if key.endswith("_FILE")]
 
-    for env_file in env_files:
+    for env_file_key in env_file_keys:
         # strip _FILE to get key name
-        key = env_file[:-5]
+        key = env_file_key[:-5]
+        env_file = os.getenv(env_file_key)
 
         logger.info(f"loading environment var {key} from {env_file}")
         try:
