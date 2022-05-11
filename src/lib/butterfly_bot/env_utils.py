121752a3c41010a23664a22514bb3c7f553b3ba3
Lina Edwards <lina@butterflysky.dev>
2022-05-11 09:02:06 -0700

    fix typo

--- src/lib/butterfly_bot/env_utils.py
+++ src/lib/butterfly_bot/env_utils.py
@@ -1,6 +1,6 @@
+import contextlib
 import os
 
-import contextib
 from dotenv import load_dotenv
 
 
@@ -8,7 +8,7 @@ def load_environment():
     load_dotenv()
     env_files = [key for key in os.environ.key() if key.endswith("_FILE")]
 
-    with contextib.suppress(Exception):
+    with contextlib.suppress(Exception):
         for env_file in env_files:
             # strip _FILE to get key name
             key = env_file[:-5]
